e7916a79342b237443c6ae32727255e147790624
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-07-18 19:06:16 +0200

    adding more meow and css to navbar

--- client/src/components/Navlinks.jsx
+++ client/src/components/Navlinks.jsx
@@ -5,16 +5,17 @@ function playRandomMeow() {
   const randomNum = Math.floor(Math.random() * (meows.length - 0));
   const randomMeow = meows[randomNum];
   new Audio(randomMeow).play();
+  console.log(randomMeow)
 }
 
 function Navlinks() {
   return (
-    <>
+    <div className='buttons'>
       <button onClick={playRandomMeow}>Meow</button>
       <Link to={'/game'}><button>Play</button></Link>
       <Link to={'/scoreboard'}><button>Scoreboard</button></Link>
       <Link to={'/user-profile'}><button>User Profile</button></Link>
-    </>
+    </div>
   )
 }
 
--- client/src/components/audio.js
+++ client/src/components/audio.js
@@ -3,7 +3,12 @@ import catGrowl from '../audio/cat-growl.mp3';
 import catMeow from '../audio/cat-meow.mp3';
 import fijiMeow from '../audio/fiji-meow.mp3';
 import funnyMeow from '../audio/funny-meow.mp3';
+import beefMeow from '../audio/beef_mow.mp3';
+import shortMeow from '../audio/cat_meow_once.mp3';
+import loudMeow from '../audio/cat-meow-loud.mp3';
+import luckyMeow from '../audio/lucky-meowing.mp3';
 
-const meows = [angryMeow, catGrowl, catMeow, fijiMeow, funnyMeow];
+
+const meows = [angryMeow, catGrowl, catMeow, fijiMeow, funnyMeow, beefMeow, shortMeow, loudMeow, luckyMeow];
 
 export default meows;
