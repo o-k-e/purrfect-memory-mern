f4080cf0ef59865995cf35f8a66a51cafadb419d
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-07-18 14:58:27 +0200

    reorganizing cat audio

--- client/src/components/Navlinks.jsx
+++ client/src/components/Navlinks.jsx
@@ -1,12 +1,7 @@
 import { Link } from 'react-router-dom'
-import angryMeow from '../audio/angry-cat-meow.mp3';
-import catGrowl from '../audio/cat-growl.mp3';
-import catMeow from '../audio/cat-meow.mp3';
-import fijiMeow from '../audio/fiji-meow.mp3';
-import funnyMeow from '../audio/funny-meow.mp3';
+import meows from './audio';
 
 function playRandomMeow() {
-  const meows = [angryMeow, catGrowl, catMeow, fijiMeow, funnyMeow];
   const randomNum = Math.floor(Math.random() * (meows.length - 0));
   const randomMeow = meows[randomNum];
   new Audio(randomMeow).play();
---
+++ client/src/components/audio.js
@@ -0,0 +1,9 @@
+import angryMeow from '../audio/angry-cat-meow.mp3';
+import catGrowl from '../audio/cat-growl.mp3';
+import catMeow from '../audio/cat-meow.mp3';
+import fijiMeow from '../audio/fiji-meow.mp3';
+import funnyMeow from '../audio/funny-meow.mp3';
+
+const meows = [angryMeow, catGrowl, catMeow, fijiMeow, funnyMeow];
+
+export default meows;
