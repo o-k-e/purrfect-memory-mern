26600fdc67f8467f42eb53e434a8749e5567823a
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-27 18:44:29 +0200

    style: fix fetched pic size

--- client/src/components/game/Card.jsx
+++ client/src/components/game/Card.jsx
@@ -7,14 +7,14 @@ function Card({ cat, width, backOfImage, height, handleClick, selected, stopFlip
         <div className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${selected ? "rotate-y-180" : ""}`}>
           {/* Front image */}
           <img
-            className="absolute w-full h-full backface-hidden rounded-md"
+            className="absolute w-full h-full object-cover backface-hidden rounded-md"
             src={backOfImage}
             alt="Back of card"
             onClick={() => !stopFlip && handleClick(cat)}
           />
           {/* Back image */}
           <img
-            className="absolute w-full h-full backface-hidden rotate-y-180 rounded-md"
+            className="absolute w-full h-full object-cover backface-hidden rotate-y-180 rounded-md"
             src={cat.img}
             alt="Front of card"
           />
