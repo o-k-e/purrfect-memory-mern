e46426b47867335a60f96fb07a2c9608f8efdad6
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-27 16:20:47 +0200

    design: change logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
-import catlogo from '../images/cat_logo.png';
+import catlogo from '../images/cat-logo.png';
 
 function Logo() {
 	return (
-<div className="h-32 w-32 flex items-center justify-center">
+<div className="h-24 w-24 flex items-center justify-center">
       <Link to="/welcome">
         <img src={catlogo} alt="cat logo" className="object-contain" />
       </Link>
