6913d5898d6c11894b020db5858f0fa634f1d70c
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-09-12 17:11:44 +0200

    add cors to server.js file

--- server/server.js
+++ server/server.js
@@ -2,11 +2,31 @@ import express from 'express';
 import { connectDB } from './db.js';
 import userRoutes from './routes/userRoutes.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log("cors");
+    console.log(origin);
+
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.json());
 
